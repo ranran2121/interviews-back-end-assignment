60b8ff48c321c62636edd0499baa0b72022e80f0
francesca <francesca.giammichele@etiqa.it>
2024-04-02 00:01:00 +0200

    CH1-add pagination to improve performance

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -4,7 +4,16 @@ import { productList } from "../mocks/productList";
 const productRoutes = express.Router();
 
 productRoutes.get("/", async (req: Request, res: Response) => {
-  return res.status(200).json(productList);
+  const page = parseInt((req.query?.page as string) ?? "0", 10);
+  const limit = parseInt((req.query?.limit as string) ?? "100", 10);
+
+  const pagedList = productList.slice(page * limit, (page + 1) * limit);
+
+  return res.status(200).json({
+    total: productList.length,
+    totalPages: Math.ceil(productList.length / limit),
+    products: pagedList,
+  });
 });
 
 export default productRoutes;
