9efab6a29b533ae6a6805af1b6e0e957f1a9dec3
francesca <francesca.giammichele@etiqa.it>
2024-04-01 22:41:42 +0200

    CH1-add rate limit to improve performance

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+
 import bodyParser from "body-parser";
 import productRoutes from "./routes/products";
+import { limiter } from "./middleware/rateLimit";
 
 const app = express();
+
+app.use(limiter);
 
 app.use(bodyParser.json({ limit: "50mb", type: "application/json" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
